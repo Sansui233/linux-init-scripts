c7c3d302f3249f377317a99c269c60bf15cdc711
Sansui233 <sansuilnm@gmail.com>
2021-11-14 16:18:28 +0800

    mkdir release

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 WD=$(cd "$(dirname "$0")";pwd)
 DIRNAME=${WD##*/}
 
+if [[ ! -d $WD/release/ ]]; then mkdir -p $WD/release/; fi
+
 cd $WD; cd ..
 tar -czf $DIRNAME.tar.gz $DIRNAME
 mv $DIRNAME.tar.gz $WD/release/
