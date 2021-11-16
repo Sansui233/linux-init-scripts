a48ba249666c8efee7b46c1ee5965e3678ebd3c1
Sansui233 <sansuilnm@gmail.com>
2021-11-16 18:14:17 +0800

    fix autojump

--- install/autojump.sh
+++ install/autojump.sh
@@ -1,13 +1,14 @@
 ######## Install autojump
 if [[ ! -d "/$USER/autojump" ]]; then
     echo -e '\033[34mInstalling autojump... \033[0m'
+    cd /$USER/
     git clone git://github.com/joelthelion/autojump.git
     cd autojump
     if command -v python >/dev/null 2>&1; then
         echo 'autojump dependecy: python is already installed'
     else
         echo -e 'autojump dependecy: \033[34mInstalling python dependency...\033[0m'
-        apt install python
+        $INSTALLER install python
     fi
     ./install.py # or ./uninstall.py
     echo "[[ -s /$USER/.autojump/etc/profile.d/autojump.sh ]] && source /$USER/.autojump/etc/profile.d/autojump.sh" >>/$USER/.bash_profile
---
+++ install_optional/telegram-cli_conf/autotg.sh
@@ -0,0 +1,2 @@
+# auto checkin. You can add this task to cron table
+/root/tg/bin/telegram-cli -W -e "msg channelName /checkin"
