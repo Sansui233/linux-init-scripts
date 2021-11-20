3f37f5f2f2a6d826d990e162f1c9e7e66c90a3e6
Sansui233 <sansuilnm@gmail.com>
2021-11-20 15:46:54 +0800

    update release

--- release.sh
+++ release.sh
@@ -3,6 +3,10 @@ DIRNAME=${WD##*/}
 
 if [[ ! -d $WD/release/ ]]; then mkdir -p $WD/release/; fi
 
+chmod +x $WD/install/*.sh
+chmod +x $WD/install_optional/*.sh
+chmod +x $WD/scripts/*.sh
+
 cd $WD; cd ..
 tar -czf $DIRNAME.tar.gz $DIRNAME
 mv $DIRNAME.tar.gz $WD/release/
