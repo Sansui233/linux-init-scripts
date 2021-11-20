cec4e13153adbd8343eac4e2fd2019a6a5467d26
Sansui233 <sansuilnm@gmail.com>
2021-11-20 16:40:11 +0800

    trojan

--- install_optional/trojan.sh
+++ install_optional/trojan.sh
@@ -2,4 +2,5 @@ mkdir /$USER/trojan-go
 cd /$USER/trojan-go
 wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
 unzip trojan-go-linux-amd64.zip && rm trojan-go-linux-amd64.zip
-cp -r $WD/install_optional/trojan-conf/. /$USER/trojan-go/
+cp $WD/install_optional/trojan-conf/config.json /$USER/trojan-go/
+cp $WD/install_optional/trojan-conf/trojan.conf /etc/nginx/sites-available/
