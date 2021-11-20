63e3e490b1a89c39f135bda5966e43d450368843
Sansui233 <sansuilnm@gmail.com>
2021-11-20 15:41:14 +0800

    update private info

--- install_optional/telegram-cli_conf/autotg.sh
+++ install_optional/telegram-cli_conf/autotg.sh
@@ -1,2 +1,3 @@
-# auto checkin. You can add this task to cron table
-/root/tg/bin/telegram-cli -W -e "msg channelName /checkin"
+# 此文件已经暂停追踪修改，后续修改不会上传到github
+# 解除：git update-index -no-assumeblablabla <filename>
+/root/tg/bin/telegram-cli -W -e "msg Pikachu_|_xsDoge /checkin"
