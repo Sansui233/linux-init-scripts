322f7d38f14358283dea5e01eb89c45cd1ba3d9b
Sansui233 <sansuilnm@gmail.com>
2022-05-26 16:28:18 +0800

    subconv

---
+++ install_optional/subconverter.sh
@@ -0,0 +1,5 @@
+cd /$USER
+wget https://github.com/tindy2013/subconverter/releases/latest/download/subconverter_linux64.tar.gz -O subconverter.tar.gz
+tar -xvf subconverter.tar.gz
+rm subconverter.tar.gz
+cd $WD
