010d6e71c825f318eee42e267deb83646e6db4de
Sansui233 <sansuilnm@gmail.com>
2022-06-01 02:17:52 +0800

    updt

--- install_optional/trojan.sh
+++ install_optional/trojan.sh
@@ -1,8 +1,12 @@
 mkdir /$USER/trojan-go
 cd /$USER/trojan-go
-wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
+wget https://github.com/p4gefau1t/trojan-go/releases/latest/download/trojan-go-linux-amd64.zip
 unzip trojan-go-linux-amd64.zip && rm trojan-go-linux-amd64.zip
 cp $WD/install_optional/trojan-conf/config.json /$USER/trojan-go/
 cp $WD/install_optional/trojan-conf/trojan.conf /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/trojan.conf /etc/nginx/sites-enabled/trojan.conf
-printYellow "[TODO] modify your config in /etc/nginx/sites-enabled/"
+printYellow "[TODO] Modify your config in /etc/nginx/sites-enabled/"
+
+sudo cp $WD/install_optional/trojan-conf/trojan.service /etc/systemd/system/
+sudo systemctl daemon-reload
+printBlue "[Info] Trojan-go is installed in /$USER/trojan-go"
